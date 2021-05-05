82fc96ca8d4e854211af3370e84d1efd704fc835
gwarz <gmwarzecha@tutanota.com>
2021-05-04 22:19:22 -0700

    set up App.js with various Apollo components

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+// a React component that is used to provide data to all of the other components
+import { ApolloProvider } from '@apollo/react-hooks';
+// gets the data when we're ready to use it
+import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
