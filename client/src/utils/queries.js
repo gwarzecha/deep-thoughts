d2e473e2f59978ef3e0ce246d4debc219a703926
gwarz <gmwarzecha@tutanota.com>
2021-05-08 22:12:15 -0700

    added profile customization for logged in and logged out users, more authentication implementation

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -56,3 +56,47 @@ export const QUERY_USER = gql`
     }
   }
 `;
+
+// this query essentially retrieves all data related to a logged-in user
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// returns a little less data for the homepage than QUERY_ME 
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
